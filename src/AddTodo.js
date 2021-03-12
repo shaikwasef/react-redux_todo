a34beb80862abd3d31b9fb225a06d38de9b80fca
shaikwasef <wasefshaik@gmail.com>
2021-03-12 16:38:15 +0530

    2 files modified

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -2,17 +2,20 @@ import React from "react";
 import "./style.css";
 import { useRef } from "react";
 import {useSelector,useDispatch} from  "react-redux"
+import addToDo from "./action/addToDo";
+
 //id for new todo item (updated in  button after each submit)
 var nextToDoID = 0;
 
 export default function AddTodo({onAddtodoClick}) {
+  const dispatch = useDispatch();
   const inputField = useRef("");
   return (
     <div>
       <input ref={inputField} />
       <button
         onClick={() => {
-          onAddtodoClick(inputField.current.value, nextToDoID++);
+          dispatch(inputField.current.value, nextToDoID++);
           inputField.current.value = "";
         }}
       >
--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ export default function App() {
   return (
     <div>
       <AddTodo
+
       />
 
       <TodoList/>
