87680c91896e429f6d5c384df08db4bd2c09d6d8
shaikwasef <wasefshaik@gmail.com>
2021-03-12 13:29:28 +0530

    1 file modified

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,7 +6,7 @@ export default function TodoList({visibleTodo,onTodoClick}) {
   return (
     <ul>
     {visibleTodo.map((todo) => {return  (
-    <Todo key={todo.id} onClick = {onTodoClick(todo.id)} {...todo}/>);})}
+    <Todo key={todo.id} onClick = {() => {onTodoClick(todo.id)}} {...todo}/>);})}
     </ul>
   );
 }
